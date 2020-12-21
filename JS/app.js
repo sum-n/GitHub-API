f4b1fd09bfcdf8d42ea1b0793dd89009f7ae2650
Suman Neupane <neupanes@tcd.ie>
2020-12-21 22:09:31 +0000

    Display user info

--- JS/app.js
+++ JS/app.js
@@ -17,10 +17,32 @@ form.addEventListener('submit', function(e) {
 		console.log(data)
 		loadingElement.style.display = '';
 
-	//Display the user avatar
-	document.getElementById("result").innerHTML = `
-	<a target="_blank" href="https://github.com/${search}"> <img src="${data.avatar_url}"/></a>
-	`
+	//Displaying the user information
+	let image = document.getElementById("result");
+	image.innerHTML = `<a target="_blank" href="https://github.com/${search}"> <img src="${data.avatar_url}"/></a>`
+	
+    let name = document.getElementById('name');
+    name.innerHTML = `<b>Name: </b>${data.name}`;
+
+    let login = document.getElementById('login');
+    login.innerHTML = `<b>Login ID: </b>${data.login}`;
+
+    let bio = document.getElementById('bio');
+    bio.innerHTML = `<b>Bio: </b>${data.bio == null ? 'User hasn\'t set a bio :(' : data.bio}`;
+
+    let followers = document.getElementById('followers');
+    followers.innerHTML = `<b>Followers: </b>${data.followers}`;
+
+    let following = document.getElementById('following');
+	following.innerHTML = `<b>Following: </b>${data.following}`;
+	
+	let public_repos = document.getElementById('public_repos');
+	public_repos.innerHTML = `<b>Public Repos: </b>${data.public_repos}`;
+
+    let location = document.getElementById('location');
+    location.innerHTML = `<b>Location: </b>${data.location}`
+	
 })
+
 
 })
